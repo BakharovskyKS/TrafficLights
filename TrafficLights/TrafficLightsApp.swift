6a087db2d6d7e3266f9fcf0e022b2fd766518bbe
Kirill Bakharovsky <baharovsky@bk.ru>
2021-11-12 00:56:51 +0500

    Initial Commit

---
+++ TrafficLights/TrafficLightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightsApp.swift
+//  TrafficLights
+//
+//  Created by Кирилл Бахаровский on 11/12/21.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
